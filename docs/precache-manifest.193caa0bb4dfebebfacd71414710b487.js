98b3fa38bbb67025e4a6477adf85429f9d4d8277
phonglk <phong.lekhoi@gmail.com>
2020-02-29 00:10:47 +0800

    chore: fix static page

---
+++ docs/precache-manifest.193caa0bb4dfebebfacd71414710b487.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e7f7741b60ad0fb9d53a98b5b5441e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae03a79973c39b5c0354",
+    "url": "/static/css/main.ddd606a5.chunk.css"
+  },
+  {
+    "revision": "50c82f9581a755c9340f",
+    "url": "/static/js/2.c6da9941.chunk.js"
+  },
+  {
+    "revision": "8fc8974dc2d3536ed9f3a5920e69acad",
+    "url": "/static/js/2.c6da9941.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae03a79973c39b5c0354",
+    "url": "/static/js/main.cd1dc97e.chunk.js"
+  },
+  {
+    "revision": "8229932f0e7feab9a2d1",
+    "url": "/static/js/runtime-main.b09ea13e.js"
+  }
+]);
